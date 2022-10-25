4156da026ace7676b9a0e829d76014f5056ba8d9
yogadharshan <yogadharshan007@gmail.com>
2022-10-25 12:41:42 +0530

    Edit Run.py

---
+++ Run.py
@@ -0,0 +1,10 @@
+#South Indian Meal Decision Maker
+
+"""
+It gets the input from the user to predict what to cook whether 
+in lunch or (breakfast or dinner)
+"""
+
+food = int(input("Enter-1 to predict for breakfast or dinner\
+    \nEnter-2 to predict for lunch\
+        \nType 1 or 2:"))
