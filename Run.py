f63777a0786d1285062b806373d9d13fdae4fc63
yogadharshan <yogadharshan007@gmail.com>
2022-10-25 17:44:28 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,4 @@
-#Meal Decision Maker
+#South Indian Meal Decision Maker
 
 """
 It gets the input from the user to predict what to cook whether 
@@ -17,22 +17,26 @@ chutney =["Tomato Chutney"]
 food_list1 = break_and_dinner_foods
 
 def random_element(x):
-    return x[rm.randint(0 , len(x) - 1, 1)]
+    a = rm.randint(0 , len(x-1), 1)
+    return x[a]
 #A loop to process the given inputs
+# Asking the user to enter the number of recipes they want to enter.
 if food == 3:
     no_of_recipes = int(input("Enter no. of recipes you are going to enter:"))
     for i in range(no_of_recipes):
-        lunch_or_break = int(input("Enter 1 for lunch recipe or \n\
-            Enter-2 for breakfast or dinner:\n Type 1 or 2:"))
+        lunch_or_break = int(input("Enter 1 for lunch recipe or\
+            \nEnter-2 for breakfast or dinner:\n Type 1 or 2:"))
         if lunch_or_break == 1:
             side_dish = input("Enter a side dish for lunch:")
             side_dish_lunch.append(side_dish)
+            lunch_dish = input("Enter lunch:")
+            lunch_foods.append(lunch_dish)        
         elif lunch_or_break == 2:
             chutney_input = input("Enter Chutney:")
             chutney.append(chutney_input)
         else:
             print("Invalild number, You have typo.")
-    print("Thank you for giving us recipes")
+    print("Thank you! for giving us recipes")
 elif food == 2:
     print("The recipe you can prepare for now is\
         ",random_element(lunch_foods),"with",random_element(side_dish_lunch))
