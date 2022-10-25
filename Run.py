52b4693ae5dc068f400c743066c8fc20fab345c1
yogadharshan <yogadharshan007@gmail.com>
2022-10-25 12:54:24 +0530

    Adding dictionary of South Indian foods

--- Run.py
+++ Run.py
@@ -1,4 +1,4 @@
-#South Indian Meal Decision Maker
+#Meal Decision Maker
 
 """
 It gets the input from the user to predict what to cook whether 
@@ -8,3 +8,5 @@ in lunch or (breakfast or dinner)
 food = int(input("Enter-1 to predict for breakfast or dinner\
     \nEnter-2 to predict for lunch\
         \nType 1 or 2:"))
+
+dict_foods = { 1 : "Dosa", 2 : "Idly", 3 : "Chappathi", 3 : "Noodle",}
